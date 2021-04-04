eb59c2058435f9ce21201420ea29ed7d480d1032
Fabian Kägy <mail@fabian-kaegy.de>
2021-04-04 21:08:23 +0200

    updates

--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@
 import './scripts/functionality/syntax-highlighting';
 import './scripts/functionality/ancor-links';
 import './scripts/functionality/wp-block-gallery-lightbox';
+
+import 'normalize.css';
+
+import './index.scss';
