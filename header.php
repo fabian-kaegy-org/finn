cef36985e222d65c92f7b2a2178c0131d047fc60
Fabian Kägy <mail@fabian-kaegy.de>
2021-07-14 21:26:03 +0200

    add theme-color meta attributes

--- header.php
+++ header.php
@@ -12,6 +12,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"> 
+	<meta name="theme-color" content="#ebf0f2" media="(prefers-color-scheme: light)">
+	<meta name="theme-color" content="#454949" media="(prefers-color-scheme: dark)">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
